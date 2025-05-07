670cf2d4af769d10023c939ed9ac996c5c7bc67c
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2025-05-07 21:22:11 +0200

    Update emacs, node.

--- package-groups/development.nix
+++ package-groups/development.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: with pkgs; [
   # dev tools & editors
-  emacs29-gtk3 vim
+  emacs vim
   git git-lfs
   curl wget
   zip unzip unp
@@ -20,7 +20,7 @@
   lm_sensors
   # nice-to-have global languages
   python3
-  nodejs_18
+  nodejs_22
   # other tools
   stow
   usbutils
