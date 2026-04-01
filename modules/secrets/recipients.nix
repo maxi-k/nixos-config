4ab089d73c38c9363e4d3fa9401e92e294fdff44
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2026-04-01 17:56:28 +0200

    Add jupyter key

--- modules/secrets/recipients.nix
+++ modules/secrets/recipients.nix
@@ -1,9 +1,11 @@
 let
   thinkpad = "age14thjdmw5jqwu367fa7sdkp22ja2edj6uv4w2v77grevlarg2xyqqzfxgtj";
   luna = "age16av395mvm799asr3leu9cadsse8ccxqxrdffzyaknzxneh8d2qmssdw0d6";
+  jupyter = "age1pzffuhv963sgqswxlsn5nchx90azutw2pp4wt58tcqt5hqlm04dq3yy7qc";
 in
 {
-  all = [thinkpad luna];
+  all = [thinkpad luna jupyter];
   thinkpad = thinkpad;
   luna = luna;
+  jupyter = jupyter;
 }
